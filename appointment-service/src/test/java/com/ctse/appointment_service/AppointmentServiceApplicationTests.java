41a1d10ce182b9b2e1d7539ba278dc2aab413ee8
Sasmitha Kavindu <kavindugunasena@gmail.com>
2026-02-28 13:21:16 +0530

    chore: Spring Boot initialization
    Setup multi-module spring boot project

---
+++ appointment-service/src/test/java/com/ctse/appointment_service/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctse.appointment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ notification-service/src/test/java/com/ctse/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctse.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
