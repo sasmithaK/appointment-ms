a258be2b46c4a8ab1ef247b1995df782ee393014
Sasmitha Kavindu <kavindugunasena@gmail.com>
2026-03-05 14:07:42 +0530

    chore(booking-service): upgrade Spring Boot dependencies

--- booking-service/src/main/java/com/ctse/booking_service/model/Booking.java
+++ booking-service/src/main/java/com/ctse/booking_service/model/Booking.java
@@ -1,65 +1,18 @@
 package com.ctse.booking_service.model;
 
-import java.util.Objects;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "bookings")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class Booking {
+    @Id
 	private String id;
 	private String userId;
 	private String slotId;
 	private String status;
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getUserId() {
-		return userId;
-	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
-	public String getSlotId() {
-		return slotId;
-	}
-
-	public void setSlotId(String slotId) {
-		this.slotId = slotId;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (!(o instanceof Booking)) return false;
-		Booking booking = (Booking) o;
-		return Objects.equals(id, booking.id);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public String toString() {
-		return "Booking{" +
-				"id='" + id + '\'' +
-				", userId='" + userId + '\'' +
-				", slotId='" + slotId + '\'' +
-				", status='" + status + '\'' +
-				'}';
-	}
 }
