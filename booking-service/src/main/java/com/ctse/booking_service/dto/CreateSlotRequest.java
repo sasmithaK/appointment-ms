b006376b53f805b29adcb163f8dbb2424993745f
Sasmitha Kavindu <kavindugunasena@gmail.com>
2026-03-22 17:24:49 +0530

    chore(booking): update booking service
    Co-Authored-By: Dinithi Perera <it22642882@my.sliit.lk>
    Co-Authored-By: Isara Ekanayake <179592272+IsaraSE@users.noreply.github.com>
    Co-Authored-By: Tursh Olivea <165576182+OliveaSegaram@users.noreply.github.com>

---
+++ booking-service/src/main/java/com/ctse/booking_service/dto/CreateSlotRequest.java
@@ -0,0 +1,13 @@
+package com.ctse.booking_service.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class CreateSlotRequest {
+    private String doctorName;
+    private LocalDate date;
+    private String startTime;
+    private String endTime;
+}
