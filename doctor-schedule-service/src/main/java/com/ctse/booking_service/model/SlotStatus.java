88a35701ea2a363f6a5f0d295ff63fca3b37f848
Sasmitha Kavindu <kavindugunasena@gmail.com>
2026-03-22 16:32:26 +0530

    chore: code base clean up
    Co-Authored-By: Dinithi Perera <it22642882@my.sliit.lk>
    Co-Authored-By: Isara Ekanayake <179592272+IsaraSE@users.noreply.github.com>
    Co-Authored-By: Tursh Olivea <165576182+OliveaSegaram@users.noreply.github.com>

--- doctor-schedule-service/src/main/java/com/ctse/booking_service/model/SlotStatus.java
+++
@@ -1,6 +0,0 @@
-package com.ctse.booking_service.model;
-
-public enum SlotStatus {
-    AVAILABLE,
-    BOOKED
-}
--- doctor-schedule-service/src/main/java/com/ctse/booking_service/repository/DoctorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ctse.booking_service.repository;
-
-import com.ctse.booking_service.model.Doctor;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DoctorRepository extends MongoRepository<Doctor, String> {
-}
