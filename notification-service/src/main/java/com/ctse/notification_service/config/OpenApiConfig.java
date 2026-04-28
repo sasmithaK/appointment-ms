860dab91914c7d089612e1f8bf477cae15b450d6
Sasmitha Kavindu <kavindugunasena@gmail.com>
2026-04-28 15:23:24 +0530

    Update OpenApiConfig.java

--- notification-service/src/main/java/com/ctse/notification_service/config/OpenApiConfig.java
+++ notification-service/src/main/java/com/ctse/notification_service/config/OpenApiConfig.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// OpenAPI configuration for Notification Service API documentation
 @Configuration
 public class OpenApiConfig {
 
