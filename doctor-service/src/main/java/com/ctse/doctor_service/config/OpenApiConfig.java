7a76a3178732831a1ce02d65ad5776a50c93df1d
Sasmitha Kavindu <kavindugunasena@gmail.com>
2026-04-28 13:39:46 +0530

    Update OpenApiConfig.java

--- doctor-service/src/main/java/com/ctse/doctor_service/config/OpenApiConfig.java
+++ doctor-service/src/main/java/com/ctse/doctor_service/config/OpenApiConfig.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// Open API configuration for Doctor service API testing
 @Configuration
 public class OpenApiConfig {
 
